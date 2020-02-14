71337fd96c0e1ae11cb53393ab5568bd6bf1ccb6
ZMTZHANG <60011715+ZMTZHANG@users.noreply.github.com>
2020-02-14 09:25:53 -0800

    666

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -6,10 +6,12 @@ function setup() {
     myCanvas.parent(mySketch);
   }
   function draw() {
-    var bar = mouseX / barWidth;
-    if (bar != lastBar) {
-      var barX = bar * barWidth;
-      fill(barX, mouseY, 77);
-      rect(barX, 0, barWidth, height, 10);
-      lastBar = bar;
+
+    variableEllipse(mouseX, mouseY, pmouseX, pmouseY);
+  }
+  
+  function variableEllipse(var x, var y, var px, var py) {
+    var speed = abs(x-px) + abs(y-py);
+    stroke(speed);
+    ellipse(x, y, speed, speed);
   }
