ea221458f23d24605fdadb3ef6b3bc3fbb043101
ZMTZHANG <60011715+ZMTZHANG@users.noreply.github.com>
2020-02-14 09:32:09 -0800

    6666

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -1,17 +1,18 @@
 function setup() {
-    var myCanvas = createCanvas(800, 250);
+    var myCanvas = createCanvas();
+    createCanvas(800, 250);
     frameRate(8);
     noStroke();
     background(random(255), random(255), random(255));
     myCanvas.parent(mySketch);
   }
-  function draw() {
-
-    variableEllipse(mouseX, mouseY, pmouseX, pmouseY);
-  }
   
-  function variableEllipse(var x, var y, var px, var py) {
-    var speed = abs(x-px) + abs(y-py);
-    stroke(speed);
-    ellipse(x, y, speed, speed);
+  function draw() {
+    if(mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(random(255), random(255), random(255), random(255));
+    }
+      var circSize = random(200);
+    rect(mouseX, mouseY, circSize, circSize);
   }
